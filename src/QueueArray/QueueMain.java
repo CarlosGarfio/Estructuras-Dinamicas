55653746caf8a109e3d19165292066f3e6789c2f
carlos <carlos@carlos-pc>
2019-02-18 14:45:24 -0600

    Agremamos la clase QUEUE (falta el iterator)

---
+++ src/QueueArray/QueueMain.java
@@ -0,0 +1,86 @@
+package QueueArray;
+
+import Excepciones.IsEmptyException;
+import Excepciones.IsFullException;
+import Queue.Queue;
+import java.lang.reflect.Array;
+import java.util.Iterator;
+
+public class QueueMain<T extends Comparable<T>> implements Queue<T>, Iterable<T> {
+
+    private T[] queue;
+    private int size;
+    private int front = -1;
+    private int back = 0;
+    private int count = 0;
+    private Class<T> type = null;
+
+    public QueueMain(Class<T> clazz, int size) {
+        this.type = clazz;
+        this.size = size;
+        this.queue = (T[]) Array.newInstance(clazz, size);
+    }
+
+    @Override
+    public boolean EnQueue(T value) throws IsFullException {
+        try {
+            IsFull();
+            count++;
+            queue[back++ % size] = value;
+            return true;
+        } catch (IsFullException e) {
+            System.err.println(e.getMessage());
+            return false;
+        }
+
+    }
+
+    @Override
+    public T DeQueue() throws IsEmptyException {
+        try {
+            IsEmpty();
+            count--;
+            return queue[++front % size];
+        } catch (IsEmptyException e) {
+            System.err.println(e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public boolean RemoveAll() throws IsEmptyException {
+        front = -1;
+        back = 0;
+        count = 0;
+        return true;
+    }
+
+    @Override
+    public void IsFull() throws IsFullException {
+        if (count == size) {
+            throw new IsFullException("Full queue");
+        }
+    }
+
+    @Override
+    public T Front() throws IsEmptyException {
+        return queue[(front + 1) % size];
+    }
+
+    @Override
+    public T Last() throws IsEmptyException {
+        return queue[(back - 1) % size];
+    }
+
+    @Override
+    public void IsEmpty() throws IsEmptyException {
+        if (count == 0) {
+            throw new IsEmptyException("Empty queue.");
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
